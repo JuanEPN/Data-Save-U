471cb0aeaa6c4c3fa69c95c8e90d6c7392a89d2b
Sara1703 <sara.julissa.hernandez@correounivalle.edu.co>
2025-04-18 20:18:55 -0500

    Interfaz inicio sesión

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,10 +2,25 @@ import './login.css'
 
 const Login = () => {
     return(
-        <div className='input-container'>
-            <button className='btonUsuario'>Usuario:</button>
+        <>
+        <div className='input-group'>
+            <label htmlFor="Usuario" className='Usuario'>  Usuario:</label>
             <input type="text" className="user-text" placeholder="" />
         </ div>
+        
+        <div className='input-group'>
+            <label htmlFor="Password" >Contraseña:</label>
+            <input type="text" className="password-text" placeholder="" />
+        </div>
+        <button className='btonIniciarSesion'>Iniciar Sesión</button>
+        <div class="divider-grid">
+            <hr class="divider-line horizontal" />
+            <span class="divider-text">O</span>
+            <hr class="divider-line horizontal" />
+        </div>
+        <button className='btonCrearCuenta'>Crear una cuenta</button>
+        
+        </>
     );
   };
   
