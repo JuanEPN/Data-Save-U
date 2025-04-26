08d1e21b1d430508363239691e93530aaf7f9b32
AlejoGonzalez255 <162381038+AlejoGonzalez255@users.noreply.github.com>
2025-04-26 16:15:34 -0500

    Añadir Ver Clientes

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/login/Login";
 import Crear from "../pages/create/Crear";
 import CrearClientes from "../pages/clientes/CrearClientes/CrearClientes";
+import VerClientes from "../pages/clientes/VerCliente/VerClientes";
 
 export const Router = createBrowserRouter([
   {
@@ -15,5 +16,9 @@ export const Router = createBrowserRouter([
   {
     path: "/createClient",
     element: <CrearClientes />,
+  },
+  {
+    path: "/viewClient",
+    element: <VerClientes />,
   },
 ]);
